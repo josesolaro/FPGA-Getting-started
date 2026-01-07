98688436c885012e2005300c9d166a77359229b9
Jose Solaro <josesolaro@JoseMac.local>
2026-01-07 08:17:29 -0300

    some changes

--- main.v
+++ main.v
@@ -10,11 +10,18 @@ module main(
 
 wire w_divided_clock;
 wire w_debounced_switch;
-clock_divider #(.COUNTER(5000000)) clock_divider_int(.i_clock(i_clock), .o_clock(w_divided_clock));
-debounced #(.DEBOUNCE_LIMIT(5000000)) debounced_int(.i_clock(i_clock), 
-    .i_switch(i_switch), .o_filtered_switch(w_debounced_switch));
+clock_divider #(.COUNTER(5000000)) 
+    clock_divider_int(
+        .i_clock(i_clock), 
+        .o_clock(w_divided_clock));
 
-shifter shifter_inst(.i_clock(w_divided_clock), .i_switch(w_debounced_switch),
-                    .o_led0(o_led0),.o_led1(o_led1),.o_led2(o_led2),.o_led3(o_led3));
+debounced #(.DEBOUNCE_LIMIT(5000000)) 
+    debounced_int(
+        .i_clock(i_clock), .i_switch(i_switch), 
+        .o_filtered_switch(w_debounced_switch));
+
+shifter shifter_inst(
+    .i_clock(w_divided_clock), .i_switch(w_debounced_switch),
+    .o_led0(o_led0),.o_led1(o_led1),.o_led2(o_led2),.o_led3(o_led3));
 
 endmodule
